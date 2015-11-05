efae7804b82e0930cd46e6d4bc95019de96e5cbc
Miguel <miguelruizbas@gmail.com>
2015-11-05 18:15:06 +0100

    MUNDO 0

--- app/Http/Controllers/PdfFacturasGenerator.php
+++ app/Http/Controllers/PdfFacturasGenerator.php
@@ -6,16 +6,18 @@ use App\model\Factura;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\model\VFacturas;
 
 class PdfFacturasGenerator extends Controller
 {
 
     public function facturaNicho($id){
 
-        $factura = Factura::find($id);
+
+        $f = VFacturas::find($id);
 
 
-        $view =  \View::make('pdf.pdfcesion', compact(''))->render();
+        $view =  \View::make('pdf.pdfcesion', compact('f'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view);
         return $pdf->stream('invoice.pdf', array( 'Attachment'=>1 ));
--- resources/views/pdf/pdfcesion.blade.php
+++ resources/views/pdf/pdfcesion.blade.php
@@ -2,11 +2,17 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Factura cesion temporal</title>
+    <title>Factura cesion perpetuidad</title>
 
 </head>
 
 <style>
+
+    html, body{
+
+        width: 21cm;
+        height: 29.7cm;
+    }
 
     img{
         position: absolute;
@@ -14,9 +20,6 @@
         margin-left: -16px;
     }
 
-    #p1{
-        font-size: 9px;
-    }
 </style>
 <body>
 
@@ -27,28 +30,30 @@
 <h1 style="font-size: 18px; text-align: center "> Cementerio Eclesiástico de la Purísima Concepción</h1>
 <p id="p1">
     CIF: R3000591B <br>
-    C/. España, 3 bajo 30510 <span style="margin-left: 70%">Yecla (Murcia)</span>
+    C/. España, 3 bajo 30510 <span style="margin-left: 55%">Yecla (Murcia)</span>
 </p>
-<hr>
+<hr style="width: 100%; margin-right: 10%">
 
-<table style="width:100%">
+<?php
+
+$aux = $f->numero;
+$aux = strlen($aux);
+$aux = 5- $aux;
+?>
+
+<?php
+
+$date = new \Carbon\Carbon($f->inicio);
+$date = $date->format('j-m-Y');
+?>
+
+<table style="width:90%" border="1">
     <tr>
-        <td width="50%">Fctura nº</td>
+        <td>Fctura nº: {{$f->serie}}{{str_repeat("0", $aux)}}{{$f->numero}}-{{substr($f->inicio,0,4)}} <br>Fecha: {{$date}} </td>
+        <td><br><br>Datos del nicho</td>
         <td>Datos de facturación</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td width="60%">Fecha</td>
-        <td>Nombre y apellidos:</td>
-        <td></td>
     </tr>
 
-    <tr>
-        <td width="20%"></td>
-        <td> Datos del nicho</td>
-        <td></td>
-        <td></td>
-    </tr>
 </table>
 
 
