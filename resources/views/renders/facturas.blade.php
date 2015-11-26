35e4e83ee8109f3da17d064b9be533666f5115e9
Miguel <miguelruizbas@gmail.com>
2015-11-26 13:27:17 +0100

    MUNDO 0

--- resources/views/renders/facturas.blade.php
+++ resources/views/renders/facturas.blade.php
@@ -21,7 +21,12 @@
         $aux = $f->numero;
         $aux = strlen($aux);
         $aux = 5- $aux;
+
+        $ffin = new \Carbon\Carbon($f->fin);
+                $ffin->subYears(1);
+
         ?>
+
 
 
         <tr class="factura{{$f->id}}">
@@ -29,7 +34,7 @@
                 <td>Manteminiento Nicho</td>
                 <td>{{$f->serie}}{{str_repeat("0", $aux)}}{{$f->numero}}-{{substr($f->created_at,0,4)}}</td>
                 <td>{{$f->inicio}}</td>
-                <td>{{$f->fin - 1}}</td>
+                <td>{{$ffin}}</td>
                 <td>{{$f->nombre_titular}}</td>
                 <td>{{$f->dni_titular}}</td>
                 <td> <a href="{{ route('pdfmantenimientoNicho',[$f->id])}}"> <button class="btn btn-danger btn-xs">ver <i class="fa fa-eye fa-lg"></i></button> </a> <a onclick="dfactura({{$f->id}})"> <button class="btn btn-warning btn-xs">Eliminar <i class="fa fa-trash fa-lg"></i></button> </a></td>
